d68dee8db33a1a4e8091ebb793f75bc9a2d0e0c9
tomkentpayne <tomkentpayne@hotmail.com>
2013-12-06 12:41:02 +0000

    Complete stm32 code folder
    Ready to go

---
+++ Device Code/MAIN/Build/startup_stm32f40xx.d
@@ -0,0 +1,1 @@
+.\build\startup_stm32f40xx.o: Default\startup_stm32f40xx.s
---
+++ Device Code/MAIN/User/main.c
@@ -0,0 +1,14 @@
+#include <stm32f4xx.h>
+#include <stdio.h>
+#include <misc.h>
+#include <core_cm4.h>
+
+int main(void)
+{
+	
+	while(1)
+	{
+		
+	}
+	return 0;
+}
